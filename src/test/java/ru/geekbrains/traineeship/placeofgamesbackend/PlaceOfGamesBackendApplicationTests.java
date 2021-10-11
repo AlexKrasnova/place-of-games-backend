d7ddca3eebc655e7abc27d811d834999aeb0fafe
Alexandra Krasnova <krasnova@physics.msu.ru>
2021-10-11 16:07:05 +0300

    test CI

---
+++ src/test/java/ru/geekbrains/traineeship/placeofgamesbackend/PlaceOfGamesBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.traineeship.placeofgamesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceOfGamesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+		int i = 1;
+		assert(false);
+	}
+
+}
