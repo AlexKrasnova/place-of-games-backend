f77563947728d1cdc0fe751a31c6920e1e8916a8
Alexandra Krasnova <krasnova@physics.msu.ru>
2021-10-29 20:15:57 +0300

    POG-100. Исправлены тесты.

--- src/test/java/ru/geekbrains/traineeship/placeofgamesbackend/mapper/EventMapperUnitTest.java
+++ src/test/java/ru/geekbrains/traineeship/placeofgamesbackend/mapper/EventMapperUnitTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.geekbrains.traineeship.placeofgamesbackend.dto.EventDTO;
+import ru.geekbrains.traineeship.placeofgamesbackend.dto.EventDetailsDTO;
 import ru.geekbrains.traineeship.placeofgamesbackend.dto.PlaceDTO;
 import ru.geekbrains.traineeship.placeofgamesbackend.dto.UserDTO;
 import ru.geekbrains.traineeship.placeofgamesbackend.model.Event;
@@ -21,6 +22,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static ru.geekbrains.traineeship.placeofgamesbackend.model.Category.BASKETBALL;
 import static ru.geekbrains.traineeship.placeofgamesbackend.model.DayOfWeek.FRIDAY;
@@ -91,12 +93,6 @@ public class EventMapperUnitTest {
                         .build())
                 .when(placeMapper).mapToPlaceDTO(place);
 
-        Mockito.doReturn(UserDTO.builder()
-                        .id(user.getId())
-                        .name(user.getName())
-                        .build())
-                .when(userMapper).mapToUserDTO(user);
-
         EventDTO eventDTO = EventDTO.builder()
                 .id(event.getId())
                 .name(event.getName())
@@ -107,6 +103,90 @@ public class EventMapperUnitTest {
                 .category(event.getCategory())
                 .numberOfParticipants(event.getParticipants().size())
                 .isCurrentUserEnrolled(false)
+                .build();
+
+        Assertions.assertThat(eventMapper.mapToEventDTO(event, currentUserLogin).getId()).isEqualTo(eventDTO.getId());
+        Assertions.assertThat(eventMapper.mapToEventDTO(event, currentUserLogin).getName()).isEqualTo(eventDTO.getName());
+        Assertions.assertThat(eventMapper.mapToEventDTO(event, currentUserLogin).getTime()).isEqualTo(eventDTO.getTime());
+        Assertions.assertThat(eventMapper.mapToEventDTO(event, currentUserLogin).getDuration()).isEqualTo(eventDTO.getDuration());
+        Assertions.assertThat(eventMapper.mapToEventDTO(event, currentUserLogin).getPlace()).isEqualTo(eventDTO.getPlace());
+        Assertions.assertThat(eventMapper.mapToEventDTO(event, currentUserLogin).getMaxNumberOfParticipants()).isEqualTo(eventDTO.getMaxNumberOfParticipants());
+        Assertions.assertThat(eventMapper.mapToEventDTO(event, currentUserLogin).getCategory()).isEqualTo(eventDTO.getCategory());
+        Assertions.assertThat(eventMapper.mapToEventDTO(event, currentUserLogin).getIsCurrentUserEnrolled()).isEqualTo(eventDTO.getIsCurrentUserEnrolled());
+    }
+
+    /**
+     * Unit-тест на проверку корректной работы метода mapToEventDTO в маппере мероприятий.
+     * <p>
+     * В тесте:
+     * 1. Создается Event event и EventDTO eventDTO с соответствующими значениями полей.
+     * 2. Мочится метод mapToPlaceDTO в маппере площадок.
+     * 3. Проверяется, что результат вызова метода mapToEventDTO в маппере мероприятий совпадает с созданным в тесте eventDTO.
+     */
+    @Test
+    public void mapToEventDetailsDTOSuccess() {
+
+        String currentUserLogin = "user";
+
+        List<WorkingHours> workingHoursList = new ArrayList<>();
+        workingHoursList
+                .add(WorkingHours.builder()
+                        .dayOfWeek(FRIDAY)
+                        .startTime(LocalTime.of(10, 00))
+                        .endTime(LocalTime.of(16, 00))
+                        .build());
+
+        Place place = Place.builder()
+                .name("Стадион")
+                .address("ул. Ленина, д. 1")
+                .workingHoursList(workingHoursList)
+                .build();
+
+        User user = User.builder()
+                .id(1L)
+                .name("Вася")
+                .login("user1")
+                .password("1")
+                .build();
+        Set<User> participants = Collections.singleton(user);
+
+        Event event = Event.builder()
+                .name("Баскетбол для любителей")
+                .time(LocalDateTime.of(2021, 12, 21, 12, 00))
+                .duration(120)
+                .placeId(place.getId())
+                .place(place)
+                .maxNumberOfParticipants(12)
+                .description("Какое-то мероприятие")
+                .category(BASKETBALL)
+                .participants(participants)
+                .build();
+
+        Mockito.doReturn(PlaceDTO.builder()
+                        .id(place.getId())
+                        .name(place.getName())
+                        .address(place.getAddress())
+                        .build())
+                .when(placeMapper).mapToPlaceDTO(place);
+
+        Mockito.doReturn(UserDTO.builder()
+                        .id(user.getId())
+                        .name(user.getName())
+                        .build())
+                .when(userMapper).mapToUserDTO(user);
+
+        EventDetailsDTO eventDTO = EventDetailsDTO.builder()
+                .id(event.getId())
+                .name(event.getName())
+                .time(event.getTime())
+                .duration(event.getDuration())
+                .place(placeMapper.mapToPlaceDTO(place))
+                .maxNumberOfParticipants(event.getMaxNumberOfParticipants())
+                .description(event.getDescription())
+                .category(event.getCategory())
+                .numberOfParticipants(event.getParticipants().size())
+                .isCurrentUserEnrolled(false)
+                .participants(event.getParticipants().stream().map(userMapper::mapToUserDTO).collect(Collectors.toSet()))
                 .build();
 
         Assertions.assertThat(eventMapper.mapToEventDTO(event, currentUserLogin).getId()).isEqualTo(eventDTO.getId());
